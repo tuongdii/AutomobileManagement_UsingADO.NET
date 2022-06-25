487082fdf78c58837f8562c8a96e6720ba21d0d7
V.T.TuongDi <vothituongduy20@gmail.com>
2022-06-26 01:51:54 +0700

    Add project files.

---
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobileLibrary.BussinessObject;
+using AutomobileLibrary.DataAccess;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public void AddNew(Car car) => CarDBContext.Instance.AddNew(car);
+
+        public void Delete(int carId) => CarDBContext.Instance.Remove(carId);
+
+        public IEnumerable<Car> GetAll() => CarDBContext.Instance.GetCarList();
+
+        public Car getCarByID(int carId) => CarDBContext.Instance.GetCarByID(carId);
+
+        public void Update(Car car) => CarDBContext.Instance.Update(car);
+    }
+}
